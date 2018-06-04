b8942fe0df4f74360b17e7b5b0047c88432a346c
Florian Jacob <projects+git@florianjacob.de>
2018-06-05 00:43:53 +0200

    Properly handle CanonicalAliasEvent and NameEvent content being absent, null or empty, which is allowed by the spec to show those events were deleted: https://matrix.org/docs/spec/client_server/r0.4.0.html#m-room-canonical-alias https://matrix.org/docs/spec/client_server/r0.4.0.html#m-room-canonical-alias

--- src/room/canonical_alias.rs
+++ src/room/canonical_alias.rs
@@ -2,6 +2,7 @@
 
 use ruma_identifiers::RoomAliasId;
 use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
 
 state_event! {
     /// Informs the room as to which alias is the canonical one.
@@ -12,5 +13,26 @@ state_event! {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CanonicalAliasEventContent {
     /// The canonical alias.
-    pub alias: RoomAliasId,
+    /// Rooms with `alias: None` should be treated the same as a room with no canonical alias.
+    // The spec says “A room with an m.room.canonical_alias event with an absent, null, or empty alias field
+    // should be treated the same as a room with no m.room.canonical_alias event.”.
+    // Serde maps null fields to None by default, serde(default) maps an absent field to None,
+    // and empty_string_as_none does exactly that, preventing empty strings getting parsed as RoomAliasId.
+    #[serde(default)]
+    #[serde(deserialize_with = "empty_string_as_none")]
+    pub alias: Option<RoomAliasId>,
+}
+
+// TODO: this is a mere hack and duplicates stuff from the deserialization visitor,
+// and is not generic. There are probably other fields in need of this as well, at least there is the NameEvent.
+fn empty_string_as_none<'de, D>(de: D) -> Result<Option<RoomAliasId>, D::Error> where D: serde::Deserializer<'de> {
+    let opt: Option<String> = serde::Deserialize::deserialize(de)?;
+    match opt {
+        None => Ok(None),
+        Some(ref s) if s.is_empty() => Ok(None),
+        Some(s) => match RoomAliasId::try_from(s.as_str()) {
+            Ok(room_alias_id) => Ok(Some(room_alias_id)),
+            Err(_) => Err(serde::de::Error::invalid_value(serde::de::Unexpected::Str(s.as_str()), &"a valid RoomAliasId")),
+        }
+    }
 }
--- src/room/name.rs
+++ src/room/name.rs
@@ -11,5 +11,24 @@ state_event! {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct NameEventContent {
     /// The name of the room. This MUST NOT exceed 255 bytes.
-    pub name: String,
+    /// Rooms with `name: None` should be treated the same as a room with no name.
+    // The spec says “A room with an m.room.name event with an absent, null, or empty name field
+    // should be treated the same as a room with no m.room.name event.”.
+    // Serde maps null fields to None by default, serde(default) maps an absent field to None,
+    // and empty_string_as_none completes the handling.
+    #[serde(default)]
+    #[serde(deserialize_with = "empty_string_as_none")]
+    pub name: Option<String>,
+}
+
+// TODO: this is a mere hack and duplicates stuff from the deserialization visitor,
+// and is not generic. There are probably other fields in need of this as well, at least there is
+// the CanonicalAliasEvent.
+fn empty_string_as_none<'de, D>(de: D) -> Result<Option<String>, D::Error> where D: serde::Deserializer<'de> {
+    let opt: Option<String> = serde::Deserialize::deserialize(de)?;
+    match opt {
+        None => Ok(None),
+        Some(ref s) if s.is_empty() => Ok(None),
+        Some(s) => Ok(Some(s)),
+    }
 }
